b5193385d7c68308467d39f4c10eab17933eab01
mathewskevin <50893313+mathewskevin@users.noreply.github.com>
2019-08-16 21:18:57 -0400

    Add files via upload

---
+++ bank_database.py
@@ -0,0 +1,295 @@
+# Kevin Mathews 8/16/2019 rev 1.02
+# Bank Analysis Script
+# written in Python 3
+
+# Account statement data can be downloaded from Bank Site in 90 day chunks as CSV files.
+# The following code will generate an excel file.
+# This file will combine multiple bank statement CSV files into one list of transactions.
+# The file will also include line/pie graphs to analyze your bank statement.
+# The original CSV files will remain unmodified. Keep them for your records.
+
+import os, pdb, datetime
+import numpy as np
+import pandas as pd
+import pdb
+import xlsxwriter
+from openpyxl import Workbook
+
+# specify folder where CSV files are stored
+data_folder = 'bank_files'
+
+# specify lookup table for transaction classification
+# please update your own lookup table with transactions/categories
+lookup_file = 'lookup_table.xlsx'
+
+# specify output file name
+output_name = 'database_file (please enable editing).xlsx'
+
+data_dict = {'credit':'Transaction Date',
+			 'checking':'Date',
+			 'savings':'Date'}
+
+# function to convert date to quarter (not used)
+def quarter_col(row):
+		
+	quarter_dict = {'01':'01',
+				  '02':'01',
+				  '03':'01',
+				  '04':'02',
+				  '05':'02',
+				  '06':'02',
+				  '07':'03',
+				  '08':'03',
+				  '09':'03',
+				  '10':'04',
+				  '11':'04',
+				  '12':'04',}
+
+	month_num = row['MM']
+	year_num = str(row['Year'])[~1:]
+	quarter_name = quarter_dict[month_num]
+	
+	output_string = '20' + year_num + quarter_name
+
+	return output_string
+
+# function to generate month column (not used)
+def month_col(row):
+	month_dict = {'01':'Jan',
+				  '02':'Feb',
+				  '03':'Mar',
+				  '04':'Apr',
+				  '05':'May',
+				  '06':'Jun',
+				  '07':'Jul',
+				  '08':'Aug',
+				  '09':'Sep',
+				  '10':'Oct',
+				  '11':'Nov',
+				  '12':'Dec',}
+
+	month_num = row['MM']
+	year_num = str(row['Year'])[~1:]
+	month_name = month_dict[month_num]
+
+	output_string = '20' + year_num + month_num
+	
+	return output_string
+
+# function to add date column for sorting
+def add_date(row):
+	data_type = row['Account Type']
+	date_val = row[data_dict[data_type]]
+	date_format = datetime.datetime.strptime(date_val, '%m/%d/%Y')
+	date_month = date_format.month
+	if date_month < 10:
+		date_month = '0' + str(date_month)
+	else:
+		date_month = str(date_month)
+
+	date_day = date_format.day
+	if date_day < 10:
+		date_day = '0' + str(date_day)
+	else:
+		date_day = str(date_day)
+
+	date_year = str(date_format.year)
+	date_out = date_year + '_' + date_month + '_' + date_day
+
+	return date_out
+
+# function to clean checking account data
+def checking_convert(data):
+	debit_string = '-'
+
+	try:
+		combine_debit = data[data['Description'].str.contains(debit_string)]['Description'].str.split('- ',2,expand=True)[2]
+	except:
+		combine_debit = pd.DataFrame()
+		
+	combine_normal = data[~data['Description'].str.contains(debit_string)]['Description']
+	new_desc = pd.concat([combine_debit, combine_normal]).sort_index()
+
+	data_out = data.drop('Description', axis = 1)
+	data_out.insert(loc=2, column='Description', value=new_desc)
+
+	data_out = data_out.drop('No.', axis = 1)
+
+	assert data_out.shape[0] == data.shape[0]
+	
+	return data_out
+
+# function to clean credit account data
+def credit_convert(data):
+	data_out = data.drop('Posted Date', axis = 1) # drop posted date
+	data_out.rename(columns={'Transaction Date':'Date'}, inplace=True)# rename transaction date column
+
+	return data_out
+
+# function to clean savings account data
+def savings_convert(data):
+	data_out = data.drop('No.', axis = 1)
+
+	return data_out
+
+print('running...')
+file_list = os.listdir(data_folder)
+file_df = pd.DataFrame({'Filenames':file_list})
+lookup_table = pd.read_excel(lookup_file)
+
+# https://www.geeksforgeeks.org/python-pandas-split-strings-into-two-list-columns-using-str-split/
+new_series = file_df['Filenames'].str.split("_", n=1, expand = True)
+file_df['Type'] = new_series[0]
+
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter(output_name, engine='xlsxwriter')
+
+final_dataset = pd.DataFrame()
+for key, value in data_dict.items():
+	file_df_sort = file_df[file_df['Type']==key]
+	main_dataset = pd.DataFrame()
+	for index, row in file_df_sort.iterrows():
+		file_name = row['Filenames']
+		data_read = pd.read_csv(data_folder + '\\' + file_name)
+		main_dataset = pd.concat([main_dataset, data_read])
+
+	main_dataset['Account Type'] = key
+	date_col = main_dataset.apply(add_date, axis=1)
+	main_dataset.insert(0, 'date_col', date_col)
+	main_dataset = main_dataset.sort_values('date_col')
+	main_dataset = main_dataset.drop_duplicates()
+	
+	main_dataset = main_dataset.reset_index(drop=True)
+	
+	# edit checking dataset
+	if key == 'checking':
+		main_dataset = checking_convert(main_dataset)
+	
+	# edit credit dataset
+	if key == 'credit':
+		main_dataset = credit_convert(main_dataset)
+	
+	# edit savings dataset
+	if key == 'savings':
+		main_dataset = savings_convert(main_dataset)
+	
+	final_dataset = pd.concat([final_dataset, main_dataset], sort=True)
+	main_dataset = main_dataset.drop(['date_col', 'Account Type'],axis=1)
+
+time_df = final_dataset['date_col'].str.split('_', expand=True).drop(2, axis=1)
+time_df.columns = ['Year', 'MM']
+time_df['Month'] = time_df.apply(month_col, axis=1)
+time_df['Quarter'] = time_df.apply(quarter_col, axis=1)
+
+final_dataset = pd.concat([time_df, final_dataset], axis=1)
+
+final_dataset = final_dataset.merge(lookup_table, on='Description', how='outer')
+final_dataset = final_dataset.sort_values('date_col')
+
+try:
+	final_dataset['Year'] = final_dataset['Year'].astype('int64')
+except:
+	pdb.set_trace()
+final_dataset['Month'] = final_dataset['Month'].astype('int64')
+final_dataset['Quarter'] = final_dataset['Quarter'].astype('int64')
+final_dataset = final_dataset[['Year','Month','Date','Account Type','Category','Description','Debit','Credit']]
+
+# purchase data
+purchase_data = final_dataset[final_dataset['Account Type'].isin(['checking','credit'])]
+purchase_data = purchase_data[~purchase_data['Debit'].isnull()]
+purchase_data = purchase_data[purchase_data['Description']!='Transfer to Credit Card']
+purchase_data = purchase_data.drop('Credit', axis=1)
+
+# savings data
+savings_data = final_dataset[final_dataset['Account Type']=='savings']
+
+# chart data
+chart_data = pd.pivot_table(savings_data, values=['Debit','Credit'], index='Month', aggfunc=np.sum).reset_index()
+spending_data = pd.pivot_table(purchase_data, values=['Debit'], index='Month', aggfunc=np.sum).reset_index()
+
+chart_data = pd.merge(chart_data, spending_data, how='inner', on='Month')
+chart_data.columns = ['Month','Savings','Checking','Spent']
+
+year_cutoff = 2018
+pie_data = purchase_data[purchase_data['Year']>=year_cutoff]
+#pie_data = purchase_data
+pie_chart_data = pd.pivot_table(pie_data, values=['Debit'], index='Category', aggfunc=np.sum).reset_index().sort_values('Debit', ascending=False)
+dashboard_data = pd.pivot_table(pie_data, values=['Debit'], index='Category', aggfunc='count').reset_index()
+dashboard_data = pd.merge(dashboard_data, pie_chart_data, how='inner', on='Category')
+dashboard_data.columns = ['Category','# Purchases','$ Amount']
+dashboard_data = dashboard_data.sort_values('$ Amount', ascending=False)
+
+# paste all data
+dashboard_data.to_excel(writer, sheet_name='Dashboard', index=False, startrow=2, startcol=9)
+purchase_data.to_excel(writer, sheet_name = 'Purchase Data', index=False)
+savings_data.to_excel(writer, sheet_name = 'Savings Data', index=False)
+pie_chart_data.to_excel(writer, sheet_name='Chart Data', index=False, startrow=0, startcol=8)
+chart_data.to_excel(writer, sheet_name='Chart Data', index=False)
+
+worksheet = writer.sheets['Chart Data']
+for row in range(2, chart_data.shape[0] + 2): # sum of savings
+	formula_string = '=SUM(' + '$B$2:$B' + str(row) + ')'
+	worksheet.write_formula('E' + str(row), formula_string)
+
+	formula_string = '=SUM(' + '$C$2:$C' + str(row) + ')' # sum of spending
+	worksheet.write_formula('F' + str(row), formula_string)
+
+	formula_string = '=E' + str(row) + '-F' + str(row)# account balance
+	worksheet.write_formula('G' + str(row), formula_string)
+
+workbook  = writer.book
+worksheet = writer.sheets['Dashboard']
+worksheet.write(1,9, 'Purchase Breakdown ' + str(year_cutoff) + ' - Now')
+worksheet.set_column(9, 9, 18)
+worksheet.set_column(10, 10, 10.11)
+worksheet.set_column(11, 11, 10.11)
+
+# Create a new chart object.
+chart = workbook.add_chart({'type': 'line'})
+paste_string_1 = '=\'Chart Data\'!$G$2:$G$' + str(chart_data.shape[0] + 2) # data
+paste_string_2 = '=\'Chart Data\'!$A$2:$A$' + str(chart_data.shape[0] + 2) # labels
+chart.add_series({'values': paste_string_1, 'categories': paste_string_2, 'name':'Savings'})
+
+bar_chart = workbook.add_chart({'type': 'column'})
+paste_string_1 = '=\'Chart Data\'!$D$2:$D$' + str(chart_data.shape[0] + 2) # labels
+paste_string_2 = '=\'Chart Data\'!$A$2:$A$' + str(chart_data.shape[0] + 2) # data
+bar_chart.add_series({'values': paste_string_1, 'categories': paste_string_2, 'name':'Spending'})
+
+chart.set_title({'name': 'Bank Account'})
+chart.set_x_axis({'name':'YYYYMM', 'label_position':'low'})
+chart.set_y_axis({'name':'Dollars ($)'})
+
+chart.combine(bar_chart)
+
+# insert the chart into the worksheet.
+worksheet.insert_chart('B2', chart)
+
+# pie chart
+pie_chart = workbook.add_chart({'type':'pie'})
+val_string = '=\'Chart Data\'!$J$2:$J$' + str(pie_chart_data.shape[0] + 1)
+cat_string = '=\'Chart Data\'!$I$2:$I$' + str(pie_chart_data.shape[0] + 1)
+pie_chart.add_series({
+    'name':       'Purchases ' + str(year_cutoff) + ' - Now',
+    'categories': cat_string,
+    'values':     val_string,
+})
+
+worksheet.insert_chart('B17', pie_chart)
+
+sheet_names = ['Purchase Data','Savings Data']
+for i in sheet_names:
+	worksheet = writer.sheets[i]
+	worksheet.set_column(0, 0, 4.22)
+	worksheet.set_column(1, 1, 6.44)
+	worksheet.set_column(2, 2, 9.78)
+	worksheet.set_column(3, 3, 11.78)
+	worksheet.set_column(4, 4, 18)
+	worksheet.set_column(5, 5, 52.22)
+
+worksheet = writer.sheets['Chart Data']
+worksheet.set_column(8, 8, 18)
+
+# save database file
+writer.save()
+
+print('done.')
---
+++ bank_scrape.py
@@ -0,0 +1,299 @@
+# Kevin Mathews 8/16/2019 rev 1.02
+# Bank Analysis Web Scraper
+# written in Python 3
+
+# Automated web scraper for gathering bank statement CSVs in 90 day chunks.
+
+import webbrowser
+import config_pass
+from selenium import webdriver
+import pdb, time, sys, os
+import random, datetime
+from datetime import date, timedelta
+import pandas as pd
+
+# calculate random time to simulate human input
+def rand_time(lower, upper):
+	return random.uniform(lower, upper)
+
+# function to add date sorting column to data
+def add_date(row):
+	if data_type == 'credit':
+		date_val = row['Transaction Date']
+	else:
+		date_val = row['Date']
+	
+	date_format = datetime.datetime.strptime(date_val, '%m/%d/%Y')
+	date_month = date_format.month
+	if date_month < 10:
+		date_month = '0' + str(date_month)
+	else:
+		date_month = str(date_month)
+
+	date_day = date_format.day
+	if date_day < 10:
+		date_day = '0' + str(date_day)
+	else:
+		date_day = str(date_day)
+
+	date_year = str(date_format.year)
+	date_out = date_year + '_' + date_month + '_' + date_day
+
+	return date_out
+
+# function to rename CSV file
+def name_data(data, data_type):
+	date_col = data.apply(add_date, axis=1)
+
+	data.insert(0, 'date_col', date_col)
+	data = data.sort_values('date_col')
+
+	start_date = data.head(1)['date_col'].values[0]
+	end_date = data.tail(1)['date_col'].values[0]
+
+	output_name = data_type + '_' + str(start_date) + '-' + str(end_date) + '.csv'
+
+	return output_name
+
+# load database file and find latest item date
+def date_list():
+	print('determining dates...')
+	# find beginning dates
+	purchase_data = pd.read_excel(database_file, sheet_name = 'Purchase Data')
+	savings_data = pd.read_excel(database_file, sheet_name = 'Savings Data')
+
+	purchase_date_string = purchase_data.tail(1)['Date'].iloc[0] # latest date
+	savings_date_string = savings_data.tail(1)['Date'].iloc[0] # latest date
+
+	# determine earliest date
+	purchase_date = datetime.datetime.strptime(purchase_date_string, '%m/%d/%Y')
+	savings_date = datetime.datetime.strptime(savings_date_string, '%m/%d/%Y')
+
+	if savings_date <= purchase_date:
+		latest_date = savings_date
+	else:
+		latest_date = purchase_date
+
+	# find todays date
+	end_date = datetime.datetime.today()
+	end_date_string = end_date.strftime('%m/%d/%Y')
+
+	# loop for date intervals
+	start_date = latest_date - timedelta(days=30)
+	start_date_string = start_date.strftime('%m/%d/%Y')
+
+	mid_date = start_date + timedelta(days=round(rand_time(50,70)))
+
+	date_list = []
+	date_list.append([start_date.strftime('%m/%d/%Y'),mid_date.strftime('%m/%d/%Y')])
+	while mid_date < end_date:
+		start_date = mid_date
+		mid_date = start_date + timedelta(days=round(rand_time(50,70)))
+		start_date = start_date - timedelta(days=round(rand_time(20,30)))
+		if mid_date > end_date:
+			date_list.append([start_date.strftime('%m/%d/%Y'),end_date.strftime('%m/%d/%Y')])
+		else:
+			date_list.append([start_date.strftime('%m/%d/%Y'),mid_date.strftime('%m/%d/%Y')])
+	
+	return date_list
+
+# get all csvs for one account
+def loop_csv(data_type):
+	for dates in date_list:
+		start_date_string = dates[0]
+		end_date_string = dates[1]
+		
+		download_button = ''
+		time.sleep(rand_time(0.5,2.0))
+		while download_button == '':
+			try:
+				download_button = browser.find_element_by_link_text('DOWNLOAD')
+			except:
+				download_button = ''
+
+		time.sleep(rand_time(0.5,2.0))
+		download_button.click()
+
+		time.sleep(rand_time(0.5,2.0))
+		dropdown_button = ''
+		while dropdown_button == '':
+			try:
+				time.sleep(rand_time(0.5,2.0))
+				dropdown_button = browser.find_elements_by_xpath("//span[@class='k-dropdown-wrap k-state-default']")
+				assert len(dropdown_button) >= 2
+				assert dropdown_button[len(dropdown_button) - 1].text == '-- Select --'
+				dropdown_button = dropdown_button[len(dropdown_button) - 1]
+			except:
+				dropdown_button = ''
+
+		time.sleep(rand_time(0.5,2.0))
+		dropdown_button.click()
+
+		dropdown_csv = ''
+		while dropdown_csv == '':
+			try:
+				dropdown_csv = browser.find_elements_by_xpath("//*[contains(text(),'CSV')]")
+			except:
+				dropdown_csv = ''
+
+		dropdown_csv_button = ''
+		for i in range(0, len(dropdown_csv)):
+			try:
+				assert dropdown_csv[i].text == 'CSV - Comma Separated Value'
+				dropdown_csv_button = dropdown_csv[i]
+			except:
+				pass
+
+		if dropdown_csv_button == '':
+			pdb.set_trace()
+
+		time.sleep(rand_time(0.5,2.0))
+		dropdown_csv_button.click()
+
+		download_button = browser.find_element_by_xpath("//button[@class='button orange account-downloadtransactions-download-button']")
+		start_time = browser.find_element_by_xpath("//input[@data-val-greaterthan-field='MinDateRangeFrom']")
+		end_time = browser.find_elements_by_xpath("//input[@data-val-lessthanorequal='Ending date must be earlier than today.']")[1]
+
+		start_time.clear()
+		end_time.clear()
+
+		start_time.send_keys(start_date_string)
+		end_time.send_keys(end_date_string)
+
+		time.sleep(rand_time(0.5,2.0))
+
+		download_button.click()
+
+		#loop until download finished		
+		while not os.path.exists(src):
+			time.sleep(0.25)
+
+		time.sleep(0.5)
+
+		# generate file name
+		try:
+			data = pd.read_csv(src)
+		except:
+			print('error.')
+			pdb.set_trace()
+
+		output_name = name_data(data, data_type)
+		dst = download_folder + '\\' + output_name
+
+		#move transactions file
+		try:
+			os.rename(src, dst)
+		except:
+			print('error.')
+			pdb.set_trace()
+
+		time.sleep(0.5)
+		
+# input variables
+site_name = 'https://www.somebankwebsite.com/' # site name
+profile_loc = r'C:\Users\Bob\AppData\Roaming\Mozilla\Firefox\Profiles\r5s0g7v9.default' # web browser profile
+credit_name = 'Alice' # credit account name
+checking_name = 'Bob' # checking account name
+savings_name = 'Eve' # savings account name
+
+download_folder = r'C:\Users\Bob\Downloads'
+database_folder = r'---'
+database_file = database_folder + '\\database_file (please enable editing).xlsx'
+src = download_folder + '\\transactions.CSV'
+
+# determine date_list
+date_list = date_list()
+
+# check if there really is a need to update
+if len(date_list) < 2:
+	pdb.set_trace()
+
+# get webpage
+print('logging in...')
+web_page = site_name
+profile = profile_loc
+
+browser = webdriver.Firefox(profile)
+browser.get(web_page)
+	
+# enter password
+user_name = config_pass.BANK_USERNAME
+user_pass = config_pass.BANK_PASSWORD
+
+loginElem = browser.find_element_by_name('user')
+loginElem.send_keys(user_name)
+
+time.sleep(rand_time(0.5,2.0))
+passwordElem = browser.find_element_by_name('password')
+passwordElem.send_keys(user_pass)
+time.sleep(rand_time(0.5,2.0))
+
+loginButton = browser.find_element_by_css_selector('input[title=\'Sign Into Online Banking\']')
+loginButton.click()
+
+# get information
+time.sleep(rand_time(1.0,2.0))
+
+# https://stackoverflow.com/questions/34759787/fetch-all-href-link-using-selenium-in-python
+a_list = []
+update_num = 0
+while len(a_list) == 0:
+	a_list = browser.find_elements_by_xpath("//a[@href]")
+	update_num = update_num + 1
+	update_string = '\r' + str(update_num) + ' ' + str(len(a_list))
+	sys.stdout.write(update_string)
+	sys.stdout.flush()
+
+print('')
+a_list = browser.find_elements_by_xpath("//a[@href]")
+checking_account = ''
+savings_account = ''
+credit_account = ''
+
+while credit_account == '':
+	try:
+		credit_account = browser.find_element_by_link_text(credit_name)
+	except:
+		credit_account = ''
+
+time.sleep(rand_time(1.0,2.0))
+
+accountButton = browser.find_element_by_css_selector('a[id=\'lnkAccountSummary\']')
+
+pdb.set_trace()
+
+# credit account
+data_type = 'credit'
+time.sleep(rand_time(1.0,2.0))
+credit_account.click()
+loop_csv(data_type)
+
+# checking account
+data_type = 'checking'
+time.sleep(rand_time(0.5,2.0))
+accountButton = browser.find_element_by_css_selector('a[id=\'lnkAccountSummary\']')
+accountButton.click()
+time.sleep(rand_time(0.5,2.0))
+checking_account = browser.find_element_by_link_text(checking_name)
+checking_account.click()
+loop_csv(data_type)
+
+# savings account
+data_type = 'savings'
+time.sleep(rand_time(0.5,2.0))
+accountButton = browser.find_element_by_css_selector('a[id=\'lnkAccountSummary\']')
+accountButton.click()
+time.sleep(rand_time(0.5,2.0))
+savings_account = browser.find_element_by_link_text(savings_name)
+savings_account.click()
+loop_csv(data_type)
+
+# sign output_name
+time.sleep(rand_time(0.5,2.0))
+signOutButton = browser.find_element_by_css_selector('a[id=\'SingOutLnk\']')
+signOutButton.click()
+time.sleep(rand_time(0.5,2.0))
+
+# close browser
+browser.close()	
+print('done.')
